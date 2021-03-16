ffb7b8c97c9d0317fedd9fdf561fe7daf4c08be0
ayoubkandah <a.kandah994@gmail.com>
2021-03-16 21:44:15 +0200

    dd

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs")
 app.use(server.urlencoded({ extended: true }))
 app.use(override("method"))
 // const client = new pg.Client(process.env.DATABASE)
-const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+const client = new pg.Client({ connectionString: process.env.DATABASE, ssl: { rejectUnauthorized: false } });
 client.connect()
 app.listen(port, () => {
     console.log(port);
